609dca97d02edc118994c02518d1c11c6e643f37
Bolzingerjo <jonathan.bolzinger@gmail.com>
2022-06-27 16:41:21 +0200

    Partie 5
    accessibilité vidéo

--- scripts/factories/photographerPageFactories.js
+++ scripts/factories/photographerPageFactories.js
@@ -1,3 +1,4 @@
+// factory header //
 function photographerFactory(data) {
     const { name, portrait, city, country, tagline, price, id } = data;
     console.log(data);
@@ -31,7 +32,8 @@ function photographerFactory(data) {
     }
     return { getUserCardDOM }
 };
-
+// factory header //
+// factory media //
 function mediaFactory(data1) {
     const { photgrapherId, image, video, title, likes, date, price, id } = data1;
     console.log(data1);
@@ -41,6 +43,7 @@ function mediaFactory(data1) {
     function getMediaCardDOM() {
         if (object = image) {
             const articlePhoto = document.createElement('article');
+            articlePhoto.className = 'article-Photo';
             const photos = document.createElement('img');
             photos.setAttribute("src", pictures);
             photos.setAttribute("role", 'link');
@@ -56,31 +59,173 @@ function mediaFactory(data1) {
             articlePhoto.appendChild(photos);
             articlePhoto.appendChild(text);
             articlePhoto.appendChild(cptLikes);
+
             return (articlePhoto);
         }
-        if (Object = video) {
+        if (object = video) {
             const articleVideo = document.createElement('article');
             const videoPage = document.createElement('video');
-            videoPage.setAttribute("controls", 'controls');
+            // videoPage.setAttribute("controls", 'controls');
+            videoPage.className = 'video';
             const source = document.createElement('source');
             source.setAttribute("src", videos);
-            // source.setAttribute("type", 'video/mp4');
+            source.setAttribute("type", 'video/mp4');
+            source.setAttribute("preload", 'auto');
+            source.className = 'source';
+            const ariaControls = document.createElement('div');
+            ariaControls.className = 'controls';
+            const btnPlay = document.createElement('button');
+            btnPlay.className = 'play';
+            btnPlay.setAttribute("data-icon", 'P');
+            btnPlay.setAttribute("aria-label", 'Play Pause Toggle');
+            const btnStop = document.createElement('button');
+            btnStop.className = 'stop';
+            btnStop.setAttribute("data-icon", 'S');
+            btnStop.setAttribute("aria-label", 'Stop');
+            const timer = document.createElement('div');
+            timer.className = 'timer';
+            const div = document.createElement('div');
+            const span = document.createElement('span');
+            span.setAttribute("aria-label", 'timer');
+            span.textContent = '00:00';
+            const rewind = document.createElement('button');
+            rewind.className = 'rwd';
+            rewind.setAttribute("data-icon", 'B');
+            rewind.setAttribute("aria-label", 'Rewind');
+            const forward = document.createElement('button');
+            forward.className = 'fwd';
+            forward.setAttribute("data-icon", 'F');
+            forward.setAttribute("aria-label", 'Fast forward');
 
             articleVideo.appendChild(videoPage);
             videoPage.appendChild(source);
+            articleVideo.appendChild(ariaControls);
+            ariaControls.appendChild(btnPlay);
+            ariaControls.appendChild(btnStop);
+            ariaControls.appendChild(timer);
+            timer.appendChild(div);
+            timer.appendChild(span);
+            ariaControls.appendChild(rewind);
+            ariaControls.appendChild(forward);
+
             return (articleVideo);
+
         } else {
             return console.log('erreur')
         }
-
-
-
-
-        // article.appendChild(videoPage);
-
-        // article.appendChild(cptLikes);
-
-
     }
+
     return { getMediaCardDOM }
 };
+// factory media //
+
+// gestion video //
+// JS //
+const gstVideo = document.querySelector('.video');
+const controls = document.querySelector('.controls');
+
+const play = document.querySelector('.play');
+const stop = document.querySelector('.stop');
+const rwd = document.querySelector('.rwd');
+const fwd = document.querySelector('.fwd');
+
+const timerWrapper = document.querySelector('.timer');
+const timer = document.querySelector('.timer span');
+const timerBar = document.querySelector('.timer div');
+
+// gstVideo.removeAttribute('controls');
+controls.style.visibility = 'visible';
+
+play.addEventListener('click', playPauseMedia);
+stop.addEventListener('click', stopMedia);
+gstVideo.addEventListener('ended', stopMedia);
+rwd.addEventListener('click', mediaBackward);
+fwd.addEventListener('click', mediaForward);
+gstVideo.addEventListener('timeupdate', setTime);
+
+function playPauseMedia() {
+    rwd.classList.remove('active');
+    fwd.classList.remove('active');
+    clearInterval(intervalRwd);
+    clearInterval(intervalFwd);
+    if (gstVideo.paused) {
+        play.setAttribute('data-icon', 'u');
+        gstVideo.play();
+    } else {
+        play.setAttribute('data-icon', 'P');
+        gstVideo.pause();
+    }
+}
+
+function stopMedia() {
+    gstVideo.pause();
+    gstVideo.currentTime = 0;
+    rwd.classList.remove('active');
+    fwd.classList.remove('active');
+    clearInterval(intervalRwd);
+    clearInterval(intervalFwd);
+    play.setAttribute('data-icon', 'P');
+}
+
+let intervalFwd;
+let intervalRwd;
+
+function mediaBackward() {
+    clearInterval(intervalFwd);
+    fwd.classList.remove('active');
+
+    if (rwd.classList.contains('active')) {
+        rwd.classList.remove('active');
+        clearInterval(intervalRwd);
+        gstVideo.play();
+    } else {
+        rwd.classList.add('active');
+        gstVideo.pause();
+        intervalRwd = setInterval(windBackward, 200);
+    }
+}
+
+function mediaForward() {
+    clearInterval(intervalRwd);
+    rwd.classList.remove('active');
+
+    if (fwd.classList.contains('active')) {
+        fwd.classList.remove('active');
+        clearInterval(intervalFwd);
+        gstVideo.play();
+    } else {
+        fwd.classList.add('active');
+        gstVideo.pause();
+        intervalFwd = setInterval(windForward, 200);
+    }
+}
+
+function windBackward() {
+    if (gstVideo.currentTime <= 3) {
+        stopMedia();
+    } else {
+        gstVideo.currentTime -= 3;
+    }
+}
+
+function windForward() {
+    if (gstVideo.currentTime >= gstVideo.duration - 3) {
+        stopMedia();
+    } else {
+        gstVideo.currentTime += 3;
+    }
+}
+
+function setTime() {
+    const minutes = Math.floor(gstVideo.currentTime / 60);
+    const seconds = Math.floor(gstVideo.currentTime - minutes * 60);
+
+    const minuteValue = minutes.toString().padStart(2, '0');
+    const secondValue = seconds.toString().padStart(2, '0');
+
+    const mediaTime = `${minuteValue}:${secondValue}`;
+    timer.textContent = mediaTime;
+
+    const barLength = timerWrapper.clientWidth * (gstVideo.currentTime / gstVideo.duration);
+    timerBar.style.width = `${barLength}px`;
+}
