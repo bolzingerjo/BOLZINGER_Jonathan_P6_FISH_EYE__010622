e0cbd86f6e5c7f31f6176c74b226523f4c621c11
Bolzingerjo <jonathan.bolzinger@gmail.com>
2022-07-18 20:49:22 +0200

    Partie 6
    corrections

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,4 +1,4 @@
-const focusableSelector = 'close-modal, input, contact-button';
+const focusableSelector = 'img, input, button';
 let focusables = [];
 let previouslyFocusedElement = null;
 const modal = document.getElementById("contact_modal");
@@ -30,7 +30,7 @@ const stopPropagation = function(e) {
 };
 
 const focusInModal = function(e) {
-    e.preventdefault()
+    e.preventDefault()
     let index = focusables.findIndex(f => f === modal.querySelector(':focus'))
     if (e.shiftKey === true) {
         index--
@@ -52,5 +52,107 @@ window.addEventListener('keydown', function(e) {
     }
     if (e.key === 'Tab') {
         focusInModal(e)
+    }
+});
+
+// DOM ELEMENTS
+const forName = document.querySelector(".prénom");
+const birthName = document.querySelector(".nom");
+const email = document.querySelector(".email");
+
+//Valider le prénom
+forName.addEventListener('change', function() {
+    validForName(this);
+});
+const validForName = function(inputForName) {
+    let smallForName = document.querySelector("#prenom-small");
+    // console.log(inputForName);
+    // console.log(inputForName.value.length);
+    if (inputForName.value.length >= 2) {
+        // smallForName.innerHTML = 'Prénom valide'
+        // smallForName.style.color = "green"
+        smallForName.innerHTML = ''
+        return true
+    } else {
+        smallForName.innerHTML = 'le prénom doit contenir au moins 2 lettres'
+        smallForName.style.color = "red"
+        return false
+    }
+};
+
+//Valider le nom
+birthName.addEventListener('change', function() {
+    validBirthName(this);
+});
+const validBirthName = function(inputBirthName) {
+    let smallBirthName = document.querySelector("#nom-small");
+    // console.log(inputBirthName);
+    // console.log(inputBirthName.value.length);
+    if (inputBirthName.value.length >= 2) {
+        // smallBirthName.innerHTML = 'Nom valide'
+        // smallBirthName.style.color = "green"
+        smallBirthName.innerHTML = ''
+        return true
+    } else {
+        smallBirthName.innerHTML = 'Le nom doit contenir au moins 2 lettres'
+        smallBirthName.style.color = "red"
+        return false
+    }
+};
+
+//Regex email
+email.addEventListener('change', function() {
+    validEmail(this);
+});
+// création regex pour validation email
+const validEmail = function(inputEmail) {
+    let emailRegex = new RegExp('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$');
+    let testEmail = emailRegex.test(inputEmail.value);
+    let small = document.querySelector("#email-small");
+    // console.log(testEmail);
+    // console.log(inputEmail.value);
+    if (emailRegex.test(inputEmail.value)) {
+        // small.innerHTML = 'Adresse valide'
+        // small.style.color = "green"
+        small.innerHTML = ''
+        return true
+    } else {
+        small.innerHTML = 'Adresse non valide'
+        small.style.color = "red"
+        return false
+    };
+};
+
+//vider la modal
+
+const btnSubmit = document.querySelector(".contact_button");
+const form = document.querySelector("form");
+const modalContact = document.querySelector('.modal');
+
+btnSubmit.addEventListener('click', function(event) {
+    event.preventDefault();
+    let errors = 0;
+    if (!validForName(forName)) {
+        errors++;
+    }
+    if (!validEmail(email)) {
+        errors++;
+    }
+    if (!validBirthName(birthName)) {
+        errors++;
+    }
+    if (errors == 0) {
+        form.innerHTML = "";
+        let p = document.createElement('p');
+        p.innerHTML = "Merci pour votre inscription";
+        let link = document.createElement('img');
+        link.classList.add('close-modal');
+        link.setAttribute('src', "assets/icons/close.svg");
+        content.classList.replace("content", "content2");
+        link.addEventListener('click', function() {
+            modalBg.style.display = "none";
+        })
+        modalContact.append(p);
+        modalContact.append(link);
     }
 });
