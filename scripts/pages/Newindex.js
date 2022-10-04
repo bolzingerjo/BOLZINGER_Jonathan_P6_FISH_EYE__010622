88b4cd2314b53262b64e28492706a891632f0084
Bolzingerjo <jonathan.bolzinger@gmail.com>
2022-10-04 18:40:21 +0200

    Partie 8
    Refacto

--- scripts/pages/Newindex.js
+++ scripts/pages/Newindex.js
@@ -1,88 +1,109 @@
-function getDataJSOn(data) {
-    const data = { name, portrait, city, country, tagline, price, id };
+function photographerFactory(data) {
+    const { name, portrait, city, country, tagline, price, id } = data;
+    console.log(data)
     const picture = `assets/photographers/${portrait}`;
-};
 
-function createArticlePhotographe() {
-    const article = document.createElement('article');
-};
+    let text = document.createElement('p');
 
-function createLienVersPagePhotographe(data) {
-    const link = document.createElement('a');
-    link.className = 'link';
-    link.href = "photographer.html?id=" + id;
-};
+    function createLienPagePhotographe() {
+        const link = document.createElement('a');
+        link.className = 'link';
+        link.href = "photographer.html?id=" + id;
+        link.appendChild(createIMG());
+        link.appendChild(createTitreNom());
+        return link
+    };
 
-function createImageProfilPhotographe(data) {
-    const img = document.createElement('img');
-    img.setAttribute("src", picture)
-    img.setAttribute("role", 'link')
-    img.setAttribute("alt", name + ', from ' + city + ', ' + country + ', citation : ' + tagline + ', ' + price + '€ par jour')
-    img.className = 'img_index';
-};
+    function createIMG() {
+        const img = document.createElement('img');
+        img.setAttribute("src", picture)
+        img.setAttribute("role", 'link')
+        img.setAttribute("alt", name + ', from ' + city + ', ' + country + ', citation : ' + tagline + ', ' + price + '€ par jour')
+        img.className = 'img_index';
+        return img
+    };
 
-function createNamePhotographe(data) {
-    const h2 = document.createElement('h2');
-    h2.textContent = name;
-    h2.className = 'name';
-};
+    function createTitreNom() {
+        const h2 = document.createElement('h2');
+        h2.textContent = name;
+        h2.className = 'name';
+        return h2
+    };
 
-function createText() {
-    document.createElement('p');
-};
+    function createTextVille() {
+        const where = document.createElement('p');
+        where.textContent = city + ', ' + country;
+        where.className = 'where';
+        return where
+    };
 
-function createPaysPhotographe(data) {
-    createText(where)
-    where.textContent = city + ', ' + country;
-    where.className = 'where';
-};
+    function createTextTag() {
+        const tag = document.createElement('p');
+        tag.textContent = tagline;
+        tag.className = 'tag';
+        return tag
+    };
 
-function createTagPhotographe(data) {
-    createText(tag)
-    tag.textContent = tagline;
-    tag.className = 'tag';
-};
+    function createTextTarif() {
+        const tarif = text;
+        tarif.textContent = price + '€/jour';
+        tarif.className = 'tarif';
+        return tarif
+    };
 
-function createTarifPhotographe(data) {
-    createText(tarif)
-    tarif.textContent = price + '€/jour';
-    tarif.className = 'tarif';
+    function createIndexDOM() {
+        const article = document.createElement('article');
+        article.append(createLienPagePhotographe());
+        article.append(createTextVille());
+        article.append(createTextTag());
+        article.append(createTextTarif());
+        return article
+    };
+    return { createIndexDOM }
 };
-createArticlePhotographe();
-createLienVersPagePhotographe(data);
-createImageProfilPhotographe(data);
-createNamePhotographe(data);
-createPaysPhotographe(data);
-createTagPhotographe(data);
-createTarifPhotographe(data);
-
-function createDomIndex(data) {
-    article.appendChild(link);
-    link.appendChild(img);
-    link.appendChild(h2);
-    article.appendChild(where);
-    article.appendChild(tag);
-    article.appendChild(tarif);
-};
-createDomIndex(data);
-
-function getJSONDataPhotographers() {
+async function getPhotographers() {
+    // Penser à remplacer par les données récupérées dans le json
     const photographers =
-        fetch('./data/photographers.json')
+        await fetch('./data/photographers.json')
         .then((Response) => Response.json())
         .then(data => data.photographers)
-        // console.log(photographers);
+
+    // console.log(photographers);
+    // et bien retourner le tableau photographers seulement une fois
     return ({
         photographers: photographers
     })
+
 };
 
-function createEachBlockPhotographe(photographers) {
+async function displayData(photographers) {
     const photographersSection = document.querySelector(".photographer_section");
+
     photographers.forEach((photographer) => {
-        const userCardDOM = photographerModel.createDomIndex(photographer);
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.createIndexDOM();
         photographersSection.appendChild(userCardDOM);
     });
 };
-getJSONDataPhotographers();
-createEachBlockPhotographe(photographers);
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
+};
+
+init();
+
+// //rajout d'attributs d'accessibilité
+function accessibilitéIndex() {
+    const indexNav = document.querySelector('.photographer_section');
+    indexNav.setAttribute("aria-label", 'photographers navigation');
+    const indexLogo = document.querySelector('.logo');
+    indexLogo.setAttribute("alt", 'Fisheye home page');
+    const linkIndex = document.createElement('a');
+    const head = document.querySelector('header');
+    head.appendChild(linkIndex);
+    linkIndex.appendChild(indexLogo);
+    linkIndex.href = "index.html";
+};
+accessibilitéIndex();
